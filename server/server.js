b3f4002a2ee767f8a1fa9fb2a8bba1b27a8dfc4a
Phạm Quang Tú <166728868+Tupham2k4@users.noreply.github.com>
2026-05-19 19:01:17 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import { inngest, functions } from "./inngest/index.js";
 const app = express();
 await connectDB();
 app.use(express.json());
-app.use(cors();
+app.use(cors());
 app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 const PORT = process.env.PORT || 4000;
