fc7723c7b02e068ca0357c481405a48208b71c73
Phạm Quang Tú <166728868+Tupham2k4@users.noreply.github.com>
2026-05-21 18:56:30 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,13 +17,9 @@ app.use(clerkMiddleware());
 app.get("/", (req, res) => res.send("Server is running"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/user", userRouter);
-<<<<<<< HEAD
 app.use("/api/post", postRouter);
 app.use("/api/story", storyRouter);
 app.use("/api/message", messageRouter);
-=======
-
->>>>>>> eb09b8edbc0eefaf5c6a2b3058c2cd2c44951bd0
 let _dbConnected = false;
 async function ensureDbConnection() {
   if (_dbConnected) return;
